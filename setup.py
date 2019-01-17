a5019ac3b4cfa1ba5425124b36b706476b4b00d3
Karl Martin Teras <karl.martin@ter.as>
2019-01-17 22:36:48 +0200

    Changed to pyinstaller

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-import sys
+import subprocess
+import os
 
-from cx_Freeze import setup, Executable
-
-options = {"include_files": ["timewire/res"]}
-
-base = None
-
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-setup(
-    name="Timewire",
-    version="0.1",
-    description="App for tracking time",
-    options={"build_exe": options},
-    executables=[Executable("timewire/main.py", base=base)]
-)
+if __name__ == '__main__':
+    subprocess.run([
+        'pyinstaller',
+        'timewire/main.py',
+        '--add-data', f'timewire/res/style{os.pathsep}res/style',
+        '--name', 'timewire',
+        '-y'
+    ])
