6018f59de629f6280f2ea21f6194c9edd0d482cf
Karl Martin Teras <karl.martin@ter.as>
2019-07-02 19:51:31 +0300

    Fixed Windows idle tracker and lockfile

--- timechart/core/idle_detector.py
+++ timechart/core/idle_detector.py
@@ -9,7 +9,8 @@ class IdleDetector(metaclass=Singleton):
             from timechart.core.linux import idle_detector_linux
             self.is_idle = idle_detector_linux.is_idle
         elif sys.platform == 'win32':
-            pass
+            from timechart.core.win32 import idle_detector_win32
+            self.is_idle = idle_detector_win32.is_idle
 
     def is_idle(self) -> bool:
         return self.is_idle()
