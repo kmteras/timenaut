30ea1ebe6679fea2135813331ec998896a11ca71
Karl Martin Teras <karl.martin@ter.as>
2019-08-06 21:58:20 +0300

    Added config and files for build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
 module.exports = {
     pluginOptions: {
         electronBuilder: {
-            mainProcessWatch: ['src/**/*.ts', 'src/**/*.vue']
+            mainProcessWatch: ['src/**/*.ts', 'src/**/*.vue'],
+            builderOptions: {
+                productName: "Timechart",
+                appId: "io.timechart.timechart",
+                linux: {
+                    category: "Utility"
+                },
+                fileAssociations: [
+                    {
+                        ext: "icon",
+                        icon: "icons/"
+                    }
+                ]
+            }
         }
     }
 };
