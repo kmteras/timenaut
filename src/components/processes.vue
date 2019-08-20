3da0f3f36b1021f41f89862cfcd5c170db4b6064
Karl Martin Teras <karl.martin@ter.as>
2019-08-20 21:59:44 +0300

    Added window table

--- src/components/processes.vue
+++ src/components/processes.vue
@@ -1,21 +1,45 @@
 <template>
     <div id="processes">
         <div class="section" id="infoSection">
-            <h1>Processes</h1>
+            <div v-if="selectedWindow !== undefined">
+                <p>{{"Window: " + selectedWindow.title}}</p>
+            </div>
+            <div v-else-if="selectedProcess !== undefined">
+                <p>{{"Path: " + selectedProcess.path}}</p>
+            </div>
+            <div v-else>
+            </div>
         </div>
-        <div class="section" id="processesSection">
-            <div id="processData">
-                <table class="table is-narrow">
+        <div class="section" id="tableSection">
+            <div id="processTableSection">
+                <table class="table is-narrow" id="processTable">
                     <thead>
                     <tr>
                         <th>Process</th>
-                        <th>Time</th>
+                        <th class="timeHeader">Time</th>
                     </tr>
                     </thead>
                     <tbody v-for="(process, idx) in this.processData" :key="idx" @click="clickProcess(process)">
-                    <tr class='hover' :class="{selected: selectedProcess === process.process_id}">
+                    <tr class='hover' :class="{selected: selectedProcessId === process.process_id}">
                         <td :style="{color: process.color}">{{process.name}}</td>
-                        <td>{{process.time}}</td>
+                        <td>{{timeAsString(process.time)}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div id="windowTableSection">
+                <table class="table is-narrow" id="windowTable">
+                    <thead>
+                    <tr>
+                        <th>Window</th>
+                        <th class="timeHeader">Time</th>
+                    </tr>
+                    </thead>
+                    <tbody v-for="(window, idx) in this.windowData" :key="idx" @click="clickWindow(window)">
+                    <tr class='hover' :class="{selected: selectedWindowId === window.window_id}">
+                        <td :style="{color: window.color}">{{window.title}}</td>
+                        <td>{{timeAsString(window.time)}}</td>
                     </tr>
                     </tbody>
                 </table>
@@ -38,26 +62,65 @@
         color: string
     }
 
+    declare interface WindowData {
+        title: string,
+        time: number,
+        window_id: number,
+        type: string,
+        color: string
+    }
+
     @Component
     export default class Processes extends Vue implements ContentPage {
         @Provide() message = 'message';
-        selectedProcess: number = -1;
+        selectedProcess?: ProcessData;
+        selectedWindow?: WindowData;
+        selectedProcessId: number = -1;
+        selectedWindowId: number = -1;
 
-        processData: [ProcessData] = this.getData();
+        processData: [ProcessData] = this.getProcessData();
+        windowData: [WindowData] = this.getWindowData();
 
         mounted() {
-            ipcRenderer.on('heartbeat', (event: any) => {
-                this.processData = this.getData();
+            ipcRenderer.on('heartbeat', () => {
+                this.processData = this.getProcessData();
+                this.windowData = this.getWindowData();
             })
         }
 
-        getData(): [ProcessData] {
+        getProcessData(): [ProcessData] {
             return ipcRenderer.sendSync('get-processes-data');
         }
 
+        getWindowData(processId: number): [WindowData] {
+            return ipcRenderer.sendSync('get-windows-data', processId);
+        }
+
         clickProcess(process: ProcessData) {
-            this.selectedProcess = process.process_id;
-            console.log(this.selectedProcess);
+            this.selectedProcess = process;
+            this.selectedProcessId = process.process_id;
+            this.selectedWindow = undefined;
+            this.selectedWindowId = -1;
+            this.windowData = this.getWindowData(this.selectedProcessId);
+        }
+
+        clickWindow(window: WindowData) {
+            this.selectedWindow = window;
+            this.selectedWindowId = window.window_id;
+        }
+
+        timeAsString(time: number): string {
+            let hours = Math.floor(time / 60 / 60);
+            let minutes = Math.floor((time - hours * 60 * 60) / 60);
+            let seconds = time - hours * 60 * 60 - minutes * 60;
+
+            if (hours > 0) {
+                return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+            } else if (minutes > 0) {
+                return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+            } else {
+                return `${seconds.toString().padStart(2, "0")}`
+            }
         }
     }
 </script>
@@ -76,10 +139,14 @@
         grid-row: 1 / 2;
     }
 
-    #processesSection {
+    #tableSection {
         grid-column: 1 / 2;
         grid-row: 2 / 3;
         overflow: auto;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+        height: 100%;
     }
 
     .section {
@@ -90,8 +157,29 @@
         background-color: white;
     }
 
-    #processData {
+    #processTableSection {
+        margin-right: 5px;
+        grid-column: 1 / 2;
         overflow: auto;
+    }
+
+    #processTable {
+        table-layout: fixed;
+    }
+
+    #windowTableSection {
+        margin-left: 5px;
+        grid-column: 2 / 3;
+        overflow: auto;
+    }
+
+    #windowTable {
+        table-layout: fixed;
+    }
+
+    td {
+        white-space: nowrap;
+        overflow: hidden;
     }
 
     tr.hover:hover {
@@ -101,5 +189,9 @@
     .selected,
     tr.hover.selected:hover {
         background-color: #C3C3C3C3;
+    }
+
+    .timeHeader {
+        width: 80px;
     }
 </style>
--- src/services/processes.ts
+++ src/services/processes.ts
@@ -6,12 +6,12 @@ import log from 'electron-log'
 export default class Processes {
 
     constructor() {
-        ipcMain.on('get-processes-data', async (event: any, arg: any) => {
+        ipcMain.on('get-processes-data', async (event: any) => {
             event.returnValue = await this.getProcessesData();
         });
 
-        ipcMain.on('get-windows-data', async (event: any, arg: any) => {
-            event.returnValue = await this.getProcessesData();
+        ipcMain.on('get-windows-data', async (event: any, processId: number) => {
+            event.returnValue = await this.getWindowData(processId);
         });
     }
 
@@ -25,15 +25,17 @@ export default class Processes {
                        pt.color
                 FROM heartbeats AS hb
                          JOIN
-                         (SELECT start_time, end_time - start_time AS difference FROM heartbeats) d
-                         ON
-                             d.start_time = hb.start_time
+                     (SELECT start_time, end_time - start_time AS difference FROM heartbeats WHERE IDLE = FALSE) d
+                     ON
+                         d.start_time = hb.start_time
                          LEFT JOIN
                      processes p on hb.process_id = p.id
                          LEFT JOIN
                      productivity_type pt on p.type_str = pt.type
-                WHERE hb.idle = FALSE AND path NOT NULL AND path <> ''
+                WHERE path NOT NULL
+                  AND path <> ''
                 GROUP BY process_id
+                HAVING time > 0
                 ORDER BY SUM(difference) DESC`);
 
             for (let result of results) {
@@ -53,6 +55,41 @@ export default class Processes {
                     result.name = result.path.match(regex)[2];
                 }
             }
+
+            return results;
+        } catch (e) {
+            log.error(e);
+        }
+    }
+
+    async getWindowData(processId: number) {
+        try {
+            let results: any = await Database.db.all(`
+                SELECT window_id,
+                       title,
+                       SUM(difference) AS time,
+                       CASE
+                           WHEN w.type_str IS NULL
+                               THEN p.type_str
+                           ELSE w.type_str
+                           END         AS type,
+                       CASE
+                           WHEN w.type_str IS NULL
+                               THEN pt.color
+                           ELSE wt.color
+                           END         AS color
+                FROM heartbeats AS hb
+                         JOIN
+                     (SELECT start_time, end_time - start_time AS difference FROM heartbeats WHERE idle = FALSE) d
+                     ON d.start_time = hb.start_time
+                         LEFT JOIN windows w ON hb.window_id = w.id
+                         LEFT JOIN processes p ON p.id = w.process_id
+                         LEFT JOIN productivity_type pt on p.type_str = pt.type
+                         LEFT JOIN productivity_type wt on w.type_str = wt.type
+                WHERE w.process_id = ?
+                GROUP BY window_id
+                HAVING time > 0
+                ORDER BY SUM(difference) DESC`, [processId]);
 
             return results;
         } catch (e) {
