9313e68e9d01d75087d8df902b5a71e20cd4962b
rebekkabb <breedis.rebekka@gmail.com>
2019-09-26 03:21:22 +0300

    Added css to settings page(inputs, layout), added a link to GitHub issue page.

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,28 +1,36 @@
 <template>
     <div id="settings">
         <div class="section">
-            <div class="settingsFlex">
-                <div class="settingsOption">
-                    <label>
-                        <input type="checkbox" @change="toggleAutostart" :checked="this.autoStartup">
-                        Start on system startup
-                    </label>
+            <div class="settingsFlexRow">
+                <h3 class="settingsTitle">Settings</h3>
+                <hr id="settingsHr">
+                <div class="settingsFlexColumn" id="main1">
+                    <div class="settingsOption">
+                        <label>
+                            <p>Start on system startup <input type="checkbox" @change="toggleAutostart" :checked="this.autoStartup"></p>
+
+
+                        </label>
+                    </div>
+                    <div class="settingsOption">
+                        <label>
+                            Poll time in seconds
+                            <input type="number" min="1" max="300" @change="changePollTime"
+                                   :value="this.getSetting('heartbeatPollTime')">
+                        </label>
+                    </div>
+                    <div class="settingsOption">
+                        <label>
+                            Idle time in seconds
+                            <input type="number" min="10" max="3600" @change="changeIdleTime"
+                                   :value="this.getSetting('heartbeatIdleTime')">
+                        </label>
+                    </div>
+
                 </div>
-                <div class="settingsOption">
-                    <label>
-                        <input type="number" min="1" max="300" @change="changePollTime"
-                               :value="this.getSetting('heartbeatPollTime')">
-                        Poll time in seconds
-                    </label>
+                <div class="settingsFlexColumn" id="main2">
                 </div>
-                <div class="settingsOption">
-                    <label>
-                        <input type="number" min="10" max="3600" @change="changeIdleTime"
-                               :value="this.getSetting('heartbeatIdleTime')">
-                        Idle time in seconds
-                    </label>
-                </div>
-                <span class="settingsFill"></span>
+                <p class="settingsLink">Found a bug? <a @click="openGitHub">Let us know!</a></p>
                 <span class="settingsVersion">v{{this.getVersion()}}</span>
             </div>
         </div>
@@ -33,6 +41,7 @@
     import {Component, Vue} from 'vue-property-decorator';
     import ipcRenderer from '@/components/ipcRenderer';
     import Updatable from "@/components/updatable";
+    import {shell} from 'electron';
 
     @Component
     export default class Settings extends Vue implements Updatable {
@@ -68,6 +77,10 @@
         protected setSetting(key: string, value: string) {
             return ipcRenderer.sendSync('set-setting', key, value);
         }
+
+        public openGitHub() {
+            shell.openExternal("https://github.com/kmteras/timenaut/issues")
+        }
     }
 </script>
 
@@ -86,15 +99,52 @@
         background-color: white;
     }
 
-    .settingsFlex {
+    .settingsFlexColumn {
         display: flex;
         flex-direction: column;
         text-align: left;
         height: 100%;
+        margin: 15px;
     }
 
-    .settingsFill {
+    .settingsFlexRow {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        grid-template-rows: 10% 86% 4%;
+        height: 100%;
+        grid-template-areas:
+                "header header"
+                "main1 main2"
+                "footer footer";
+    }
+
+    #main1 {
+        grid-area: main1;
         flex-grow: 1;
+    }
+    #main2 {
+        grid-area: main2;
+        flex-grow: 1;
+    }
+
+    #settingsHr{
+        width: 80%;
+        grid-area: header;
+        text-align: center;
+        align-items: center;
+        margin: 2rem 0 0 10% !important;
+        height: 1px;
+        background: -webkit-gradient(radial, 50% 50%, 0, 50% 50%, 350, from(gray), to(#fff));
+    }
+
+    .settingsTitle {
+        text-align: center;
+        grid-area: header;
+    }
+
+    .settingsLink {
+        text-align: left;
+        grid-area: footer;
     }
 
     .settingsOption {
@@ -104,5 +154,8 @@
 
     .settingsVersion {
         text-align: right;
+        color: #476582;
+        grid-area: footer;
+
     }
 </style>
