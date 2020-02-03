29fa8921d7067364528ab440eda432750ca2c01e
Karl Martin Teras <karl.martin@ter.as>
2020-02-04 01:09:40 +0200

    Clear tables before database test

--- src/__tests__/database.test.ts
+++ src/__tests__/database.test.ts
@@ -1,13 +1,22 @@
 import Database from "@/services/database";
 
-test("Create test database with initial tables", async () => {
+beforeEach(async () => {
+    const tables = await Database.db.all("SELECT name FROM sqlite_master WHERE type='table'");
+    for (let table of tables) {
+        await Database.db.run("DROP TABLE IF EXISTS ?", [table]);
+    }
+});
+
+test.only("Create test database with initial tables", async () => {
     // No tables initialized
     try {
         await Database.db.one('SELECT * FROM processes');
         expect(true).toBe(false);
     } catch (e) {
-        expect(e.name).toBe("SqliteError");
+        console.log(e);
+        expect(e.name).toEqual("SqliteError");
     }
+
 
     await Database.db.update();
     expect(await Database.db.all('SELECT * FROM processes')).toEqual([]);
