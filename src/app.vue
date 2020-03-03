65363f6955d75fef7b1a519a299c10e93a8d626c
Karl Martin Teras <karl.martin@ter.as>
2020-03-03 21:37:04 +0200

    Removed shadows from input elements

--- src/app.vue
+++ src/app.vue
@@ -118,7 +118,6 @@
         padding-left: 12px;
         padding-right: 12px;
         margin-left: 10px;
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
         height: 36px;
     }
 
--- src/components/fragments/form/tn_select.vue
+++ src/components/fragments/form/tn_select.vue
@@ -81,7 +81,6 @@
         border-style: solid;
         color: #2d3748;
         font-size: 14px;
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
         height: 36px;
     }
 
@@ -92,7 +91,6 @@
     .tn-select-options {
         position: absolute;
         background-color: white;
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
 
         border-width: 1px;
         border-radius: 4px;
